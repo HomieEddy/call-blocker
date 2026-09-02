8a580748ecd2ccdb187e1af0fb28e1a47c61b560
HomieEddy <mceddy95@hotmail.fr>
2026-09-02 09:38:19 -0400

    chore: add gradle build entry points

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "2.1.0" apply false
+}
+
+allprojects {
+    group = "com.teleshield"
+    version = "0.1.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "teleshield"
+
+include(":engine")
