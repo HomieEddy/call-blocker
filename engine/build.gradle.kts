c09555db0964d258c33c07f934183bac0b19245c
HomieEddy <mceddy95@hotmail.fr>
2026-09-02 09:38:19 -0400

    chore: add engine module build config

---
+++ engine/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
